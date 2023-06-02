c5717b2c626d8b5dd12e48190af13f6a7e779767
sbusisiwe21 <sbusisiwemasindane36@gmail.com>
2023-06-02 11:46:33 -0700

    Task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 
 /**
  * list_len - returns the number of the elements in a linked list
- * @h: pointer to the list_t list.
+ * @h: pointer to the list_t list
  * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
