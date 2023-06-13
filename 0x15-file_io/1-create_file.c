ad28931bdc74da4d41b326e488fbbe6652268f5a
sbusisiwe21 <sbusisiwemasindane36@gmail.com>
2023-06-13 11:03:52 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,68 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "main.h"
 
 /**
- * create_file - Create a file with the given filename and write the
- *               text_content to it.
- * @filename: The name of the file to create.
- * @text_content: The text to write to the file.
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fd, w, len = 0;
+
 	if (filename == NULL)
-		return (-1);
-
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	if (fd == -1)
-
-
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
-
-		if (bytes_written == -1)
-
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
 	close(fd);
+
 	return (1);
 }
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	const char *filename = "example.txt";
-	char *text_content = "This is an example text.";
-
-	int result = create_file(filename, text_content);
-
-	if (result == -1)
-
-	{
-		printf("Failed to create the file.\n");
-	}
-	else
-	{
-		printf("File created successfully.\n");
-	}
-
-	return (0);
-}
