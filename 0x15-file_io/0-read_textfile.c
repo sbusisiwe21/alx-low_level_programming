e67fb9efdf2ee6209973d06e5f6ff316c614b408
sbusisiwe21 <sbusisiwemasindane36@gmail.com>
2023-06-13 10:19:20 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters read and printed.
- *         0 if the file cannot be opened or read, or if filename is NULL.
- *         0 if write fails or does not write the expected amount of bytes.
+ *         0 if the file cannot be opened or read, or if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -17,41 +16,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	FILE *file = fopen(filename, "r");
 	if (file == NULL)
-
-
-
 		return (0);
 
 	char buffer[letters + 1];
-	ssize_t bytesRead = fread(buffer, sizeof(char), letters, file);
-
-	if (bytesRead == 0)
+	ssize_t read_count = fread(buffer, sizeof(char), letters, file);
+	if (read_count < 0)
 	{
 		fclose(file);
 		return (0);
 	}
 
-	buffer[bytesRead] = '\0';
+	buffer[read_count] = '\0';
 	printf("%s", buffer);
 
 	fclose(file);
-	return (bytesRead);
+	return (read_count);
 }
 
-/**
- * main - Entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	const char *filename = "example.txt";
-	size_t letters = 100;
-
-	ssize_t bytesRead = read_textfile(filename, letters);
-
-	printf("Read %ld letters from the file.\n", bytesRead);
-
-
-	return (0);
-}
