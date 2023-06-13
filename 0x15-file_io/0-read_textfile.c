a19c5876ea1a22a6417cc339eeeee225195771d5
sbusisiwe21 <sbusisiwemasindane36@gmail.com>
2023-06-13 10:55:38 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,41 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed.
+ *         0 if the file cannot be opened or read, or if filename is NULL,
+ *         or if write fails or does not write the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
-	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	if (filename == NULL)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
 
-	free(buf);
-	close(fd);
-	return (w);
+	FILE *file = fopen(filename, "r");
+
+	if (file == NULL)
+
+		return (0);
+
+	char buffer[letters + 1];
+	ssize_t read_count = fread(buffer, sizeof(char), letters, file);
+
+	if (read_count < 0)
+
+
+	{
+		fclose(file);
+		return (0);
+	}
+
+	buffer[read_count] = '\0';
+	printf("%s", buffer);
+
+	fclose(file);
+	return (read_count);
 }
 
