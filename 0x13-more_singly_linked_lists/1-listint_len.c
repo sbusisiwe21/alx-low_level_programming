73a85606575ca59249d3ca66c5d69d1fd092be76
sbusisiwe21 <sbusisiwemasindane36@gmail.com>
2023-06-25 11:49:52 -0700

    Task 1.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
+#include "lists.h"
 
-// Function to calculate the length of a list
-int listLength(int* list) {
-    int length = 0;
-    while (*list != 0) {
-        length++;
-        list++;
-    }
-    return length;
-}
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
 
-int main() {
-    int myList[] = {1, 2, 3, 4, 5, 0};  // Example list with 5 elements
-    int length = listLength(myList);
-    
-    printf("Length of the list: %d\n", length);
-    
-    return 0;
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
 }
 
